33466165e3b209fb82f9ebad63ac24217d2d2284
Muhammad Rizwan <rizwanturk!2@gmail.com>
2022-08-29 20:45:45 +0500

    code changing

--- server.mjs
+++ server.mjs
@@ -1,28 +1,30 @@
 import express from "express"
 import cors from "cors"
-import { nanoid } from 'nanoid'
-import mongoose from "mongoose";
-import { stringToHash,varifyHash, validateHash } from "bcrypt-inzi"
-
+import mongoose from 'mongoose';
+import { stringToHash, varifyHash } from "bcrypt-inzi"
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-
 const port = process.env.PORT || 3000;
 
-const userschema = new mongoose.Schema({ 
-    firstName: String, 
-    lastName: String,
-    email: String,
-    password: {type:String,required:true},
-    age: {type:Number,min:17, max:65,default:18},
-    subjects: Array,
-    isMarried: {type:Boolean,deault:false},
-    createdOn:{type: Date, default: Date.now },
- });
-const userModel = mongoose.model('newUser', userschema);
+
+const userSchema = new mongoose.Schema({
+
+    firstName: { type: String },
+    lastName: { type: String },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+
+    age: { type: Number, min: 17, max: 65, default: 18 },
+    isMarried: { type: Boolean, default: false },
+
+    createdOn: { type: Date, default: Date.now },
+});
+const userModel = mongoose.model('User', userSchema);
+
+
 
 app.post("/signup", (req, res) => {
 
@@ -44,62 +46,71 @@ app.post("/signup", (req, res) => {
         );
         return;
     }
-    //check  if user already exist // query email user
-    userModel.findOne({email:body.email},(err,data)=>{
-      if(!err)
-      {
-        console.log("data:",data);
-        if(data) //user already exist
-        {
-          console.log("user already exist:",data);
-          res.status(400).send({message:"user already exist"});
-          return;
 
-        }
-        else //user not already exist
-        {
-          stringToHash(body.password).then(hashString=>{
-            let newUser = new userModel({
-        
-              firstName: body.firstName,
-              lastName: body.lastName,
-              email: body.email.toLowerCase(),
-              password: hashString
-          });
-          newUser.save((err,result)=>{
-            if(!err){
-                //data saved
-                console.log("data saved:",result);
-                res.status(201).send({ message: "user is created" });
-                
+    // check if user already exist // query email user
+    userModel.findOne({ email: body.email }, (err, data) => {
+        if (!err) {
+            console.log("data: ", data);
+
+            if (data) { // user already exist
+                console.log("user already exist: ", data);
+                res.status(400).send({ message: "user already exist,, please try a different email" });
+                return;
+
+            } else { // user not already exist
+
+                stringToHash(body.password).then(hashString => {
+
+                    userModel.create({
+                        firstName: body.firstName,
+                        lastName: body.lastName,
+                        email: body.email.toLowerCase(),
+                        password: hashString
+                    },
+                        (err, result) => {
+                            if (!err) {
+                                console.log("data saved: ", result);
+                                res.status(201).send({ message: "user is created" });
+                            } else {
+                                console.log("db error: ", err);
+                                res.status(500).send({ message: "internal server error" });
+                            }
+                        });
+                })
+
             }
-            else
-            {
-                console.log("db error:",err);
-                res.status(500).send({message:"internal server error"});
-            }
-    
-        });
-
-          })
-
-
+        } else {
+            console.log("db error: ", err);
+            res.status(500).send({ message: "db error in query" });
+            return;
         }
-      }
     })
-
- 
-
-  
-
-    
 });
+
+
+app.get("/users", async (req, res) => {
+
+    try {
+        let allUser = await userModel.find({}).exec();
+        res.send(allUser);
+
+    } catch (error) {
+        res.status(500).send({ message: "error getting users" });
+    }
+})
+
+
+
+
+
+
+
 
 app.post("/login", (req, res) => {
 
     let body = req.body;
 
-    if (!body.email || !body.password) {
+    if (!body.email || !body.password) { // null check - undefined, "", 0 , false, null , NaN
         res.status(400).send(
             `required fields missing, request example: 
                 {
@@ -146,9 +157,12 @@ app.post("/login", (req, res) => {
 })
 
 
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+}
+)
+
 
 
 
